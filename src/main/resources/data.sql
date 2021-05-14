25aca8a1f3b0bfbc97ac3bfabcc6d5903625754d
gprajapati <gaurav.prajapati@omnibot.ai>
2021-05-14 23:23:25 +0530

    Adding project to github

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO VEHICLE (NAME, BRAND, FUEL_TYPE, MAX_SPEED) VALUES
+  ('honda city', 'four wheeler', 'petrol', '220 KM'),
+  ('verna', 'four wheeler', 'petrol', '220 km'),
+  ('XUV', 'four wheeler', 'diesel', '200 km'),
+  ('alto', 'four wheeler', 'cng', '160 km');
