171531cedef6bae6d9aed1c267474ce0bbdaf804
gprajapati <gaurav.prajapati@omnibot.ai>
2021-05-15 13:15:20 +0530

    Removed unused files.

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO VEHICLE (NAME, BRAND, FUEL_TYPE, MAX_SPEED) VALUES
-  ('honda city', 'four wheeler', 'petrol', '220 KM'),
-  ('verna', 'four wheeler', 'petrol', '220 km'),
-  ('XUV', 'four wheeler', 'diesel', '200 km'),
-  ('alto', 'four wheeler', 'cng', '160 km');
